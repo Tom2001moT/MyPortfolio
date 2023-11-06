11210e1fff4c5bde9f8b411bc4d22e6a51821eef
Thomas Francis <86661302+Tom2001moT@users.noreply.github.com>
2023-11-06 08:47:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Thomas Francis",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "tomfrancis55st2001@gmail.com",
+  gitHub: "Tom2001moT",
+  instagram: "_th._om._as_",
+  linkedIn: " ",
+  medium: " ",
+  twitter: " ",
+  youTube: "@WhiteDeath_WDG_",
 };
 
 const primaryColor = "#4E567E";
